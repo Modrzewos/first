91f028f716b34115458e27476e5a825d3434016a
Modrzewos <62855679+Modrzewos@users.noreply.github.com>
2020-03-31 22:14:06 +0200

    "rozwój_1"

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def read_item(name: str):
+    return f"Hello {name}"
