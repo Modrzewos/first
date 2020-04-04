8f11d92d0038dc1a71832ec0a187184809086c9c
Modrzewos <62855679+Modrzewos@users.noreply.github.com>
2020-04-04 22:24:41 +0200

    "first exc"

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
+# to see what funny will come
+app.counter = 0
+
+class HelloResp(BaseModel):
+    msg: str
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get("/hello/{name}")
+def counter():
+    app.counter += 1
+    return str(app.counter)
+
+@app.get("/hello/{name}", response_model = HelloResp)
 async def read_item(name: str):
-    return f"Hello {name}"
+    return HelloResp(msg=f"Hello {name}")
