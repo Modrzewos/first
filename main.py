99b9e4faee9c80acdf2dfef1a0fcd7d6c009a217
Modrzewos <62855679+Modrzewos@users.noreply.github.com>
2020-04-06 21:03:29 +0200

    "Praca domowa_1"

--- main.py
+++ main.py
@@ -1,35 +1,60 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from typing import Dict
+
+
+class Patient(BaseModel):
+    name: str
+    surename: str
+
 
 app = FastAPI()
-# to see what funny will come
-app.counter = 0
+app.counter: int = 0
+app.storage: Dict[int, Patient] = {}
 
-class HelloResp(BaseModel):
-    msg: str
 
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-def counter():
+
+@app.api_route(path="/method", methods={"GET", "POST", "DELETE", "PUT", "OPTIONS"})
+def read_request(request: Request):
+    return {"method": request.method}
+
+
+@app.post("/patient")
+def show_data(patient: Patient):
+    resp = {"id": app.counter, "patient": patient}
+    app.storage[app.counter] = Patient
     app.counter += 1
-    return str(app.counter)
-
-@app.get("/hello/{name}", response_model = HelloResp)
-async def read_item(name: str):
-    return HelloResp(msg=f"Hello {name}")
-
-class GiveMeSomethingRq(BaseModel):
-    first_key: str
+    return resp
 
 
-class GiveMeSomethingResp(BaseModel):
-    received: dict
-    constant_data: str = "python jest super"
+@app.get("/patient/{pk}")
+def show_patient(pk: int):
+    if pk in app.storage:
+        return app.storage.get(pk)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
-def receive_something(rq: GiveMeSomethingRq):
-    return GiveMeSomethingResp(received=rq.dict())
+# def counter():
+#     app.counter += 1
+#     return str(app.counter)
+#
+# @app.get("/hello/{name}", response_model = HelloResp)
+# async def read_item(name: str):
+#     return HelloResp(msg=f"Hello {name}")
+#
+# class GiveMeSomethingRq(BaseModel):
+#     first_key: str
+#
+#
+# class GiveMeSomethingResp(BaseModel):
+#     received: dict
+#     constant_data: str = "python jest super"
+#
+#
+# @app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
+# def receive_something(rq: GiveMeSomethingRq):
+#     return GiveMeSomethingResp(received=rq.dict())
